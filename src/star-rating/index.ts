10e81433e91f574a9ee1250c619389995d72b556
Michael Hladky <michael@hladky.at>
2016-10-23 21:16:38 +0200

    feat(create examples folder): refactored root src folder

---
+++ src/star-rating/index.ts
@@ -0,0 +1,15 @@
+import * as angular from 'angular';
+
+import {StarRatingComponent} from "./star-rating.component"
+import styles = require('./star-rating.sc5.scss');
+import icons = require('./icons.svg');
+
+let styles = styles;
+let icons = icons;
+
+export const angularStars = angular
+    .module('star-rating', [])
+    .component('starRating', new StarRatingComponent())
+    .name;
+
+
---
+++ src/star-rating/star-rating.component.ts
@@ -0,0 +1,35 @@
+import {StarRatingController} from "./star-rating.controller";
+import templateUrl = require('./star-rating.tpl.html');
+
+export class StarRatingComponent implements ng.IComponentOptions {
+
+    public bindings: any;
+    public controller: StarRatingController;
+    public templateUrl: string;
+    public replace: boolean;
+
+    constructor() {
+        this.bindings = {
+              id: '@'
+            , text: '<'
+            , color: '<'
+            , starType: '<'
+            , numOfStars: '<'
+            , speed : '<'
+            , size: '<'
+            , spread: '<'
+            , readOnly: '<'
+            , disabled: '<'
+            , rating: '<'
+            , labelPosition: '<'
+            , getColor: '&?'
+            , onClick: '&?'
+            , onUpdate: '&?'
+        };
+        this.replace = true;
+        this.controller = StarRatingController;
+        this.templateUrl = templateUrl;
+    }
+
+}
+
