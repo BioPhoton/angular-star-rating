abc5bf9f843d2bacd2d957ebdcd5d0ad2a5b026a
Michael Hladky <michael@hladky.at>
2016-11-05 15:32:48 +0100

    fix(ng1): added ctrl in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-//import * as angular from 'angular';
+import "angular";
 
-import {StarRatingComponent} from "./star-rating.component"
+import {StarRatingController} from "./star-rating.controller";
+import {StarRatingComponent} from "./star-rating.component";
+
 import styles = require('./scss/star-rating.sc5.scss');
 import icons = require('./star-rating.icons.svg');
-
 let styles = styles;
 let icons = icons;
 
 export const angularStars = angular
     .module('star-rating', [])
+    .controller('starRatingCtrl', StarRatingController)
     .component('starRating', new StarRatingComponent())
     .name;
-
-
