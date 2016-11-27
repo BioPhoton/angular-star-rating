afe36859c4a6a7e3cdea584f3e0fec99499a9592
Michael Hladky <michael@hladky.at>
2016-11-27 01:27:23 +0100

    chore(travis-ci): test testing

--- karma.conf.js
+++ karma.conf.js
@@ -16,14 +16,14 @@ module.exports = function (config) {
         logLevel: config.LOG_INFO,
         colors: true,
         autoWatch: true,
-        browsers: ["Chrome"],
+        browsers: ["PhantomJS"],
 
-        customLaunchers: {
+        /*customLaunchers: {
             Chrome_Travis_CI: {
                 base: 'Chrome',
                 flags: ['--no-sandbox']
             }
-        },
+        },*/
 
         files: [
             "./node_modules/angular/angular.js"
@@ -68,7 +68,7 @@ module.exports = function (config) {
     };
 
     if (process.env.TRAVIS) {
-        cfg.browsers = ['Chrome_Travis_CI'];
+        //cfg.browsers = ['Chrome_Travis_CI'];
     }
 
     config.set(cfg);
