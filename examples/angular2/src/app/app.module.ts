263a6629b7a4e1094d6d1200b54bbe7828fc0002
Michael Hladky <michael@hladky.at>
2017-07-06 03:23:09 +0200

    docs(example): updated example

--- examples/angular2/src/app/app.module.ts
+++ examples/angular2/src/app/app.module.ts
@@ -7,23 +7,30 @@ import {AppComponent} from "./app.component";
 import {FormTestComponent} from "./components/form-test/form-test.component";
 import {MyFormComponent} from "./components/my-form-component-minimal/form-test.component";
 import {MyEventsComponent} from "./components/my-events/my-events.component";
+import {MyConfigComponent} from "app/components/my-config/my-config.component";
+import {StarRatingConfig} from "angular-star-rating/dist/src/star-rating-config";
+
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormTestComponent,
-    MyFormComponent,
-      MyEventsComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    StarRatingModule,
-    ReactiveFormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FormTestComponent,
+        MyFormComponent,
+        MyEventsComponent,
+        MyConfigComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        StarRatingModule,
+        ReactiveFormsModule,
+    ],
+    providers: [
+        {provide: StarRatingConfig, useValue:{}}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
