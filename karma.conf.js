79db91d76b6715a538f8f357d2d3631b3cff7661
Michael Hladky <michael@hladky.at>
2017-07-07 21:29:06 +0200

    edit travis settings

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     customLaunchers: {
       Gitlab_CI: {
         base: 'PhantomJS'
