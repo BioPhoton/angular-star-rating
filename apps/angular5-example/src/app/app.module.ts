ac7fb122f495c97c06774924d9910fbd300cfbbc
Michael Hladky <michael@hladky.at>
2018-03-30 17:23:05 +0200

    docs(demo): updated live demo

--- apps/angular5-example/src/app/app.module.ts
+++ apps/angular5-example/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { StarRatingModule } from '@angular-star-rating-lib/angular-star-rating';
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { NxModule } from '@nrwl/nx';
-import { KitchensinkComponent } from './components/kitchensink/kitchensink.component';
-import { EventBindingsComponent } from './components/event-bindings/event-bindings.component';
-import { PropertyBindingsComponent } from './components/property-bindings/property-bindings.component';
-import { FormControlStarRatingComponent } from './components/form-control-star-rating/form-control-star-rating.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { StaticModuleModule } from './static-module/static-module.module';
+import {StarRatingModule} from '@angular-star-rating-lib/angular-star-rating';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import {NxModule} from '@nrwl/nx';
+import {AppComponent} from './app.component';
+import {EventBindingsComponent} from './components/event-bindings/event-bindings.component';
+import {FormControlStarRatingComponent} from './components/form-control-star-rating/form-control-star-rating.component';
+import {KitchensinkComponent} from './components/kitchensink/kitchensink.component';
+import {PropertyBindingsComponent} from './components/property-bindings/property-bindings.component';
+import {StaticModuleModule} from './static-module/static-module.module';
 
 const DECLARATIONS = [
   AppComponent,
@@ -26,41 +26,45 @@ const DECLARATIONS = [
     StaticModuleModule,
     StarRatingModule.forRoot(),
     RouterModule.forRoot([
+        {
+          path: '',
+          redirectTo: 'form',
+          pathMatch: 'full'
+        },
+        {
+          path: 'property-bindings',
+          component: PropertyBindingsComponent
+        },
+        {
+          path: 'event-bindings',
+          component: EventBindingsComponent
+        },
+        {
+          path: 'form-control',
+          component: FormControlStarRatingComponent
+        },
+        // static-config-override routes in its module
+        {
+          path: 'lazy-config-override',
+          loadChildren: 'apps/angular5-example/src/app/lazy-module/lazy-module.module#LazyModuleModule'
+        },
+        {
+          path: 'kitchensink',
+          component: KitchensinkComponent
+        },
+        {
+          path: '**',
+          redirectTo: 'kitchensink'
+        }
+      ],
       {
-        path: '',
-        redirectTo: 'form',
-        pathMatch: 'full'
-      },
-      {
-        path: 'property-bindings',
-        component: PropertyBindingsComponent
-      },
-      {
-        path: 'event-bindings',
-        component: EventBindingsComponent
-      },
-      {
-        path: 'form-control',
-        component: FormControlStarRatingComponent
-      },
-      // static-config-override routes in its module
-      {
-        path: 'lazy-config-override',
-        loadChildren:
-          'apps/angular5-example/src/app/lazy-module/lazy-module.module#LazyModuleModule'
-      },
-      {
-        path: 'kitchensink',
-        component: KitchensinkComponent
-      },
-      {
-        path: '**',
-        redirectTo: 'kitchensink'
+        useHash: true
       }
-    ])
+    )
   ],
   declarations: [DECLARATIONS],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
