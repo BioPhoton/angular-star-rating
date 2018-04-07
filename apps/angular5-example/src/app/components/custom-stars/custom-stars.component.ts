e2e920b6205097eb32e1220a830489b5d867b9c2
Michael Hladky <michael@hladky.at>
2018-04-07 15:07:50 +0200

    docs(github-page): update live demo

---
+++ apps/angular5-example/src/app/components/custom-stars/custom-stars.component.ts
@@ -0,0 +1,76 @@
+import {StarRatingConfigService} from '@angular-star-rating-lib/angular-star-rating';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {CustomIconsConfigService} from './custom-star-config.service';
+
+@Component({
+  selector: 'app-custom-stars',
+  templateUrl: './custom-stars.component.html',
+  styles: [`
+    .star .fas {
+      font-size: 17px !important;
+    }
+  `],
+  providers: [
+    {
+      provide: StarRatingConfigService,
+      useClass: CustomIconsConfigService
+    }
+  ],
+  encapsulation: ViewEncapsulation.None
+})
+export class CustomStarsComponent {
+
+  form: FormGroup;
+
+  icons: string[] = [
+    'fa-500px',
+    'fa-accessible-icon',
+    'fa-accusoft',
+    'fa-address-book',
+    'fa-address-card',
+    'fa-adjust',
+    'fa-adn',
+    'fa-adversal',
+    'fa-affiliatetheme',
+    'fa-algolia',
+    'fa-align-center',
+    'fa-align-justify',
+    'fa-align-left',
+    'fa-align-right',
+    'fa-allergies',
+    'fa-amazon',
+    'fa-amazon-pay',
+    'fa-ambulance',
+    'fa-american-sign-language-interpreting',
+    'fa-amilia',
+    'fa-anchor',
+    'fa-android',
+    'fa-angellist',
+    'fa-angle-double-down',
+    'fa-angle-double-left',
+    'fa-angle-double-right',
+    'fa-angle-double-up',
+    'fa-angle-down'
+  ];
+
+  constructor(fb: FormBuilder, sRCS:  StarRatingConfigService) {
+    this.form = fb.group({
+      rating: [1.5],
+      empty:[],
+      half:[],
+      filled:[]
+    });
+
+    this.form.valueChanges
+      .subscribe(
+        (formValue) => {
+          sRCS.classEmpty = formValue.half;
+          sRCS.classHalf = formValue.half;
+          sRCS.classFilled = formValue.half;
+        }
+      )
+  }
+
+
+}
